0af47f501773251452bee6b9d3aa623975c4127b
BT_3W2_11 <alps2789@nycstudents.net>
2022-10-07 12:34:26 -0400

    surely done now

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class TipCalc {
+public class TipCalc {
+    // instance variables
+    private int numPeople;
+    private double tipDecimal;
+    private double totalBillBeforeTip;
+
+    // constructor
+    public TipCalc(int numPeople, double tipDecimal) {
+        this.numPeople = numPeople;
+        this.tipDecimal = tipDecimal;
+        this.totalBillBeforeTip = 0.0;
+    }
+
+    // methods
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+
+    public double getTipDecimal() {
+        return tipDecimal;
+    }
+
+    public void addMeal(double cost) {
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount() {
+        return totalBillBeforeTip*tipDecimal;
+    }
+
+    public double totalBill() {
+        return totalBillBeforeTip+tipAmount();
+    }
+
+    public double perPersonCostBeforeTip() {
+        return totalBillBeforeTip/numPeople;
+    }
+
+    public double perPersonTipAmount() {
+        return tipAmount()/numPeople;
+    }
+
+    public double perPersonTotalCost() {
+        return totalBill()/numPeople;
+    }
 }
--- src/TipCalcRunner.java
+++ src/TipCalcRunner.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TipCalcRunner {
+import java.util.Scanner;
+public class TipCalcRunner {
+    public static void main(String[] args) {
+
+        // asking user for information
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        System.out.print("What was the total bill today? ");
+        double totalBill = scan.nextDouble();
+        System.out.print("What is the tip percentage? (As a decimal!): ");
+        double tipDecimal = scan.nextDouble();
+        System.out.print("How many people in your party this fine day? ");
+        int numPeople = scan.nextInt();
+
+        TipCalc calc = new TipCalc(numPeople, tipDecimal);
+        calc.addMeal(totalBill);
+
+        // printing results
+        System.out.println();
+        System.out.printf("Total tip amount: $%.2f\n", calc.tipAmount());
+        System.out.printf("Total bill including tip: $%.2f\n", calc.totalBill());
+        System.out.printf("Tip per person: $%.2f\n", calc.perPersonTipAmount());
+        System.out.printf("Total per person: $%.2f\n", calc.perPersonTotalCost());
+    }
 }
